4a377b054b9bdda4cd2d7490772e052f5b025dd4
Jeevitha456 <gvtha.c@gmail.com>
2020-11-23 20:21:04 +0530

    added livestock

--- app/src/main/java/com/example/fpo/fragment/AddMemberFragment/AddMemberFragment.kt
+++ app/src/main/java/com/example/fpo/fragment/AddMemberFragment/AddMemberFragment.kt
@@ -17,6 +17,8 @@ import java.util.*
 class AddMemberFragment : Fragment() {
     private lateinit var nestedScrollView: NestedScrollView
     private lateinit var linearLayout: LinearLayout
+
+    //basic profile layout
     private lateinit var edt_dob: EditText
     private lateinit var edt_gender: Spinner
     private lateinit var edt_smartphone: Spinner
@@ -27,6 +29,21 @@ class AddMemberFragment : Fragment() {
     private lateinit var basicprofileDetails: ConstraintLayout
     private lateinit var basicprofile_uparrow: ImageView
     var cal = Calendar.getInstance()
+
+    //farm layout
+    private lateinit var edt_member: Spinner
+    private lateinit var layout_farm: ConstraintLayout
+    private lateinit var layout_farmDetails: ConstraintLayout
+    private lateinit var farm_uparrow: ImageView
+
+    //livestock layout
+    private lateinit var edt_livestock__member: Spinner
+    private lateinit var edt_livestock__breed: Spinner
+    private lateinit var edt_livestock__count: Spinner
+    private lateinit var layout_livestock: ConstraintLayout
+    private lateinit var layout_livestockDetails: ConstraintLayout
+    private lateinit var livestock_uparrow: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -49,11 +66,25 @@ class AddMemberFragment : Fragment() {
         edt_uploadaddress = rootview.findViewById(R.id.upload_addressfile) as EditText
         edt_uploadidentity = rootview.findViewById(R.id.upload_identityfile) as EditText
         basicprofile = rootview.findViewById(R.id.layout_basicprofile) as ConstraintLayout
-        basicprofileDetails =
-            rootview.findViewById(R.id.layout_basicprofiledetails) as ConstraintLayout
+        basicprofileDetails = rootview.findViewById(R.id.layout_basicprofiledetails) as ConstraintLayout
         basicprofile_uparrow = rootview.findViewById(R.id.basicprofile_uparrow) as ImageView
         basicprofilelayout()
 
+        //farm layout
+        edt_member = rootview.findViewById(R.id.edt_member) as Spinner
+        layout_farm = rootview.findViewById(R.id.layout_farm) as ConstraintLayout
+        layout_farmDetails = rootview.findViewById(R.id.layout_farmdetails) as ConstraintLayout
+        farm_uparrow = rootview.findViewById(R.id.farm_uparrow) as ImageView
+        farmlayout()
+
+        //livestock
+        edt_livestock__member=rootview.findViewById(R.id.edt_livestock__member) as Spinner
+        edt_livestock__breed=rootview.findViewById(R.id.edt_livestock__breed) as Spinner
+        edt_livestock__count=rootview.findViewById(R.id.edt_livestock__count) as Spinner
+        layout_livestock = rootview.findViewById(R.id.layout_livestock) as ConstraintLayout
+        layout_livestockDetails = rootview.findViewById(R.id.layout_livestockdetails) as ConstraintLayout
+        livestock_uparrow = rootview.findViewById(R.id.livestock_uparrow) as ImageView
+        livestocklayout()
         setEventHandler()
         return rootview
     }
@@ -62,12 +93,147 @@ class AddMemberFragment : Fragment() {
         basicprofile.setOnClickListener({
             basicprofile.visibility = View.GONE
             basicprofileDetails.visibility = View.VISIBLE
+            layout_farmDetails.visibility = View.GONE
+            layout_farm.visibility = View.VISIBLE
+            layout_livestock.visibility = View.VISIBLE
+            layout_livestockDetails.visibility = View.GONE
         })
         basicprofile_uparrow.setOnClickListener({
             basicprofile.visibility = View.VISIBLE
             basicprofileDetails.visibility = View.GONE
         })
+
+        layout_farm.setOnClickListener({
+            layout_farm.visibility = View.GONE
+            layout_farmDetails.visibility = View.VISIBLE
+            basicprofileDetails.visibility = View.GONE
+            basicprofile.visibility = View.VISIBLE
+            layout_livestock.visibility = View.VISIBLE
+            layout_livestockDetails.visibility = View.GONE
+        })
+        farm_uparrow.setOnClickListener({
+            layout_farm.visibility = View.VISIBLE
+            layout_farmDetails.visibility = View.GONE
+        })
+
+        layout_livestock.setOnClickListener({
+            layout_livestock.visibility = View.GONE
+            layout_livestockDetails.visibility = View.VISIBLE
+            basicprofileDetails.visibility = View.GONE
+            basicprofile.visibility = View.VISIBLE
+            layout_farm.visibility = View.VISIBLE
+            layout_farmDetails.visibility = View.GONE
+        })
+        livestock_uparrow.setOnClickListener({
+            layout_livestock.visibility = View.VISIBLE
+            layout_livestockDetails.visibility = View.GONE
+        })
     }
+
+    private fun livestocklayout() {
+        val livestockmemberOptions = arrayOf("Ayub Khan | 9986464166", "Jeevitha | 9900765378")
+        edt_livestock__member?.adapter = activity?.applicationContext?.let {
+            ArrayAdapter(
+                it,
+                R.layout.support_simple_spinner_dropdown_item,
+                livestockmemberOptions
+            )
+        } as SpinnerAdapter
+        edt_livestock__member?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                println("erreur")
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val type = parent?.getItemAtPosition(position).toString()
+                println(type)
+            }
+
+        }
+
+        val livestockbreedOptions = arrayOf("Select type")
+        edt_livestock__breed?.adapter = activity?.applicationContext?.let {
+            ArrayAdapter(
+                it,
+                R.layout.support_simple_spinner_dropdown_item,
+                livestockbreedOptions
+            )
+        } as SpinnerAdapter
+        edt_livestock__breed?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                println("erreur")
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val type = parent?.getItemAtPosition(position).toString()
+                println(type)
+            }
+
+        }
+
+        val livestockcountOptions = arrayOf("2","3","4")
+        edt_livestock__count?.adapter = activity?.applicationContext?.let {
+            ArrayAdapter(
+                it,
+                R.layout.support_simple_spinner_dropdown_item,
+                livestockcountOptions
+            )
+        } as SpinnerAdapter
+        edt_livestock__count?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                println("erreur")
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val type = parent?.getItemAtPosition(position).toString()
+                println(type)
+            }
+
+        }
+    }
+
+    private fun farmlayout() {
+        val memberOptions = arrayOf("Ayub Khan | 9986464166", "Jeevitha | 9900765378")
+        edt_member?.adapter = activity?.applicationContext?.let {
+            ArrayAdapter(
+                it,
+                R.layout.support_simple_spinner_dropdown_item,
+                memberOptions
+            )
+        } as SpinnerAdapter
+        edt_member?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                println("erreur")
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val type = parent?.getItemAtPosition(position).toString()
+                println(type)
+            }
+
+        }
+    }
+
 
     private fun basicprofilelayout() {
         val dateSetListener = object : DatePickerDialog.OnDateSetListener {
@@ -129,7 +295,7 @@ class AddMemberFragment : Fragment() {
                 smartPhoneOptions
             )
         } as SpinnerAdapter
-        edt_smartphone?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+        edt_smartphone.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
                 println("erreur")
             }
